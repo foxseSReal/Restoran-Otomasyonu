3c082d47072cdb5d26a2a93b2413eb063df67e80
foxseSReal <207896979+foxseSReal@users.noreply.github.com>
2025-12-14 17:15:06 +0300

    Giris Window Tasarlandi

---
+++ RestoranOtomasyonu/OtherWindows/LoginWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using RestoranOtomasyonu;
+
+namespace RestoranOtomasyonu.OtherWindows
+{
+    /// <summary>
+    /// LoginWindow.xaml etkileşim mantığı
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGiris_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtUsername.Text == "a" && PasswordBox.Password == "a")
+            {
+                // Ana pencereyi aç
+                MainWindow main = new MainWindow();
+                main.Show();
+
+                // Login penceresini kapat
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Hatalı giriş");
+            }
+        }
+
+    }
+}
