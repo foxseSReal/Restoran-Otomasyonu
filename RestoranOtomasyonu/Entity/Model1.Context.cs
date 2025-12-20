3b59fec6ac090a2b7db44013527bab555fb15a00
foxseSReal <207896979+foxseSReal@users.noreply.github.com>
2025-12-21 01:52:39 +0300

    Mergeden doğan gereksiz satırlar silindi proje son haliyle düzgün çalışıyor.

--- RestoranOtomasyonu/Entity/Model1.Context.cs
+++ RestoranOtomasyonu/Entity/Model1.Context.cs
@@ -12,7 +12,8 @@ namespace RestoranOtomasyonu.Entity
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
-    
+    using System.Data.Entity.Core.Objects;
+
     public partial class RESTORANDBEntities1 : DbContext
     {
         public RESTORANDBEntities1()
