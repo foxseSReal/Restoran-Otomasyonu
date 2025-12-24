a061905ea78f436e2d2b56065442165f0c2a9f09
Gencay Çeliker <gencayceliker@gmail.com>
2025-12-24 19:27:26 +0300

    DB oluşturulması İçin nugget paketi:Microsoft.SqlServer.SqlManagementObjects indirildi

--- RestoranOtomasyonu/OtherWindows/DB.xaml.cs
+++ RestoranOtomasyonu/OtherWindows/DB.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,5 +30,50 @@ namespace RestoranOtomasyonu.OtherWindows
             this.Close();
         }
 
+        private void _DB_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                //sql server adını belirt
+                string ServerName = @".\SQLEXPRESS";
+                string dbName = "RESTORANDB";
+
+                //bağlantı nesnesi oluştur
+                ServerConnection connection = new ServerConnection(ServerName);
+                Server dbserver = new Server(connection);
+
+                //Yedekleme nesnesi oluştur
+                Backup dbBackup = new Backup
+                {
+                    Action = BackupActionType.Database,
+                    Database = dbName,
+                    Incremental = false,
+                    Initialize = true,
+                };
+
+                //Yedeğin kaydedileceği klasör
+                string klasoryolu = @"D:\DBYedek";
+
+                //Eğer bu klasör yoksa oluştur
+                if (!Directory.Exists(klasoryolu)) Directory.CreateDirectory(klasoryolu);
+
+                //Yedeği isimlendirelim
+                string dosyaadi = Path.Combine(klasoryolu, $"RESTORANDB{DateTime.Now:ddMMyyyy_HHmm}.bak");
+
+                //Yedek dosyasını klasöre ekle
+                dbBackup.Devices.AddDevice(dosyaadi, DeviceType.File);
+
+                //Yedekleme işlemini başlat
+                dbBackup.SqlBackup(dbserver);
+
+                //Bilgilendir
+                MessageBox.Show("Yedekleme işlemi tamamlandı\n" + dosyaadi, "BİLGİ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata oluştu\n" + ex.Message, "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
