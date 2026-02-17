fcbbe3f5a6faea2ec772fe5e12da7d5d4558d526
foxseSReal <207896979+foxseSReal@users.noreply.github.com>
2026-02-17 19:20:57 +0300

    Projede kullanilacak Masalar için ayrı bir Window açıldı.

--- RestoranOtomasyonu/MasalarWindow.xaml.cs
+++ RestoranOtomasyonu/MasalarWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace RestoranOtomasyonu
 {
@@ -19,9 +21,32 @@ namespace RestoranOtomasyonu
     /// </summary>
     public partial class MasalarWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
+        private readonly CultureInfo _tr = new CultureInfo("tr-TR");
+
         public MasalarWindow()
         {
             InitializeComponent();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+            _tarih.Text = DateTime.Now.ToString("D", _tr);
         }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            _saat.Text = DateTime.Now.ToString("HH:mm");
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
     }
 }
