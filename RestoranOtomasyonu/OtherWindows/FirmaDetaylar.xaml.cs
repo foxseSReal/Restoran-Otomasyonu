44edae04370ab8556b55c48528a5fcc113ef08fb
foxseSReal <207896979+foxseSReal@users.noreply.github.com>
2026-02-17 16:45:45 +0300

    Firma ve Müşteri detay pencereleri eklendi ve içerikleri düzenlend

---
+++ RestoranOtomasyonu/OtherWindows/FirmaDetaylar.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RestoranOtomasyonu.OtherWindows
+{
+    /// <summary>
+    /// FirmaDetaylar.xaml etkileşim mantığı
+    /// </summary>
+    public partial class FirmaDetaylar : Window
+    {
+        public FirmaDetaylar()
+        {
+            InitializeComponent();
+        }
+
+        private void firma_DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void urun_DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ RestoranOtomasyonu/OtherWindows/MusteriDetay.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RestoranOtomasyonu.OtherWindows
+{
+    /// <summary>
+    /// MusteriDetay.xaml etkileşim mantığı
+    /// </summary>
+    public partial class MusteriDetay : Window
+    {
+        public MusteriDetay()
+        {
+            InitializeComponent();
+        }
+
+        private void firma_DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void urun_DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
