6d776d8c96f923ea49c354d890b73e5ee03b9f13
foxseSReal <207896979+foxseSReal@users.noreply.github.com>
2026-03-24 18:54:40 +0300

    TblADISYON tablosuna SiparisTipi eklendi.

--- RestoranOtomasyonu/Entity/TblADISYON.cs
+++ RestoranOtomasyonu/Entity/TblADISYON.cs
@@ -26,6 +26,7 @@ namespace RestoranOtomasyonu.Entity
         public Nullable<System.DateTime> AcilisZamani { get; set; }
         public Nullable<System.DateTime> KapanisZamani { get; set; }
         public Nullable<bool> Durum { get; set; }
+        public string SiparisTipi { get; set; }
     
         public virtual TblMASA TblMASA { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
